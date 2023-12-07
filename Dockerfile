ea2bc13608c244b67ad300d6f15c7b8cc1c0b739
Thomas Schuetz <thomas.schuetz@whizus.com>
2023-12-07 13:02:11 +0800

    feat: update git releaser action
    Signed-off-by: Thomas Schuetz <thomas.schuetz@whizus.com>

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use the original image as a base
+FROM ghcr.io/git-releaser/git-releaser:dev-202312070426
+
+# Copy the entrypoint script into the Docker image
+COPY entrypoint.sh /entrypoint.sh
+
+# Make the entrypoint script executable
+RUN chmod +x /entrypoint.sh
+
+# Set the entrypoint script as the entrypoint for the Docker container
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Change the working directory to GITHUB_WORKSPACE
+cd "${GITHUB_WORKSPACE}" || { echo "Failed to change directory to ${GITHUB_WORKSPACE}"; exit 1; }
+
+# Check if the necessary inputs are set
+if [ -z "${INPUT_REPOSITORY}" ]; then
+  echo "INPUT_REPOSITORY is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "${INPUT_ACTOR}" ]; then
+  echo "INPUT_ACTOR is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "${INPUT_TOKEN}" ]; then
+  echo "INPUT_TOKEN is not set. Exiting."
+  exit 1
+fi
+
+Run the git-releaser update command
+/git-releaser update -r "${INPUT_REPOSITORY}" -u "${INPUT_ACTOR}" -t "${INPUT_TOKEN}" -p "${INPUT_REPOSITORY}" -g 'github' || { echo "Failed to run git-releaser update command"; exit 1; }
